1f36b5ce9da555fc592b6dfbce2758b71680eeaa
Sarthak <ytsarthak1@gmail.com>
2025-08-15 18:23:43 +0545

    tests

---
+++ main/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    int t;
+    cin >> t;
+    if (t > 0)
+        cout << t * t << endl;
+    else
+        cout << "Negative";
+    return 0;
+}
