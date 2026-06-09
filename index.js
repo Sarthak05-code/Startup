c573fec608d85138393c9207dab79135036e359d
Sarthak <218388722+Sarthak05-code@users.noreply.github.com>
2026-06-09 12:52:49 +0545

    basics

---
+++ index.js
@@ -0,0 +1,28 @@
+console.log("Name is shame ");
+
+let a;
+
+a = 10;
+
+console.log(a);
+
+var b;
+
+b = 10;
+console.log(b);
+
+
+const c = 10 ;
+
+console.log(c);
+
+// info
+
+const name = "Sarthak"
+console.log(name);
+
+const age = 20;
+console.log(age);
+
+
+
