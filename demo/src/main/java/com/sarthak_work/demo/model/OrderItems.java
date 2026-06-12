ec57a0cd8ccd837a58be9cd8e2c0758457a5abb3
Sarthak <218388722+Sarthak05-code@users.noreply.github.com>
2026-06-12 18:33:44 +0545

    work: Created the order items file and the relative infos

---
+++ demo/src/main/java/com/sarthak_work/demo/model/OrderItems.java
@@ -0,0 +1,36 @@
+package com.sarthak_work.demo.model;
+
+import java.math.BigDecimal;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Data
+@Table (name = "order_items")
+public class OrderItems {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private Integer quantity;
+
+    @Column(name = "price_at_purchase" , nullable = false)
+    private BigDecimal priceAtPurchase;
+
+
+    // relations
+    
+}
