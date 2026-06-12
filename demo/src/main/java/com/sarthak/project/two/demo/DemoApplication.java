26f5a0d0993c5b4174d754c9e1bd16a7656e9a74
Sarthak <218388722+Sarthak05-code@users.noreply.github.com>
2026-06-12 15:47:04 +0545

    checked

--- demo/src/main/java/com/sarthak/project/two/demo/DemoApplication.java
+++ demo/src/main/java/com/sarthak/project/two/demo/DemoApplication.java
@@ -1,6 +1,5 @@
 package com.sarthak.project.two.demo;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
