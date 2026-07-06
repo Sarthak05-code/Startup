74e6d7b3ba1c4342e95488f3d069b8faf5d30e42
Sarthak <218388722+Sarthak05-code@users.noreply.github.com>
2026-07-06 18:04:20 +0545

    Update main.zig

--- hello-zig/src/main.zig
+++ hello-zig/src/main.zig
@@ -1,11 +1,20 @@
 const std = @import("std");
 
-pub fn main() !void {
-    var result : usize = 1;
-    const number : usize = 4;
-    for(0..number + 1) |i| { // since i dont know how to do <= , ill use the + 1 tactic
-        result *= i;
-    }
-    std.debug.print("{}\n", .{result});
+pub fn main(init : std.process.Init) !void {
+    
+        const gpa = init.gpa;
+    
+
+        const args = try init.minimal.args.toSlice(init.arena.allocator());
+
+        std.log.info("Program has {d} args", .{args.len});
+        for (args, 0..) |arg , i| {
+            std.log.info(" args[{d}] = {s}  ", .{i , arg});
+        }
+
+        std.log.info("{d} env vars available", .{init.environ_map.count()});
+        const greeting = try std.fmt.allocPrint(gpa, "Hello {s}!", .{"Zig 0.16.0"});
+
+        defer gpa.free(greeting);
     
 }
