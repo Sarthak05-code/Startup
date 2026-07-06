cea9f1c6d0ab5890421e2f6d1252799b66887788
Sarthak <218388722+Sarthak05-code@users.noreply.github.com>
2026-07-06 14:11:14 +0545

    factorial

--- hello-zig/src/main.zig
+++ hello-zig/src/main.zig
@@ -1,16 +1,11 @@
 const std = @import("std");
 
-
 pub fn main() !void {
-
-    const year = 2002;
-
-    if((year % 400 == 0) or (year % 100 != 0 and year % 4 == 0)) {
-        std.debug.print("The year is a leap year", .{});
-    } else {
-        std.debug.print("The year ain't a leap year", .{});
+    var result : usize = 1;
+    const number : usize = 4;
+    for(0..number + 1) |i| { // since i dont know how to do <= , ill use the + 1 tactic
+        result *= i;
     }
-        
-    
+    std.debug.print("{}\n", .{result});
     
 }
