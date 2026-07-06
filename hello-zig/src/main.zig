b27542bd42667ab41b63a3fd9efc575bde9493b8
Sarthak <218388722+Sarthak05-code@users.noreply.github.com>
2026-07-06 14:04:14 +0545

    leap year

--- hello-zig/src/main.zig
+++ hello-zig/src/main.zig
@@ -1,25 +1,16 @@
 const std = @import("std");
 
-fn checker(x: i32) []const u8 {
-    return switch (x) {
-        0 => "Zero",
-        else => if (@mod(x, 2) == 0)
-            "Even"
-        else
-            "Odd",
-    };
-}
-
-fn fibonacci(x: i32) i32 {
-    if (x <= 1) // 0 and 1
-        return x;
-    return fibonacci(x - 2) + fibonacci(x - 1);
-}
 
 pub fn main() !void {
-    const array: [10]u8 = .{ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
 
-    for (array) |arr| {
-        std.debug.print("{} => {s} and Fibonacci-Sequence => {}\n", .{ arr, checker(arr), fibonacci(arr) });
+    const year = 2002;
+
+    if((year % 400 == 0) or (year % 100 != 0 and year % 4 == 0)) {
+        std.debug.print("The year is a leap year", .{});
+    } else {
+        std.debug.print("The year ain't a leap year", .{});
     }
+        
+    
+    
 }
