6bc1fbbe7d7a6b6d388719b96580364f1a58bdde
Sarthak <218388722+Sarthak05-code@users.noreply.github.com>
2026-07-18 16:27:41 +0545

    stuffs.

--- main.cc
+++ main.cc
@@ -3,13 +3,14 @@
 
 using namespace std;
 int main(void) {
-    cout << "Testing Standalone Inline Allocator\n";
-    int* array = static_cast<int*>(custom_malloc(sizeof(int) * 20));
-    if(array != nullptr) {
-        for (int i = 0 ; i < 5 ; ++i) {
-            array[i] = (i + 1) * 10;
-            cout << "Element " << i << ": " << array[i] << " at " << &array[i] << "\n";
-        }
-        custom_free(array);
+  cout << "Testing Standalone Inline Allocator\n";
+  int *array = static_cast<int *>(custom_malloc(sizeof(int) * 20));
+  if (array != nullptr) {
+    for (int i = 0; i < 5; ++i) {
+      array[i] = (i + 1) * 10;
+      cout << "Element " << i << ": " << array[i] << " at " << &array[i]
+           << "\n";
     }
+    custom_free(array);
+  }
 }
