6b8c00f17dd16602d69444b2bb6687247615f77d
Sarthak <218388722+Sarthak05-code@users.noreply.github.com>
2026-08-01 18:12:34 +0545

    Struct and Impl usecase

--- Rustwork/src/pin.rs
+++ Rustwork/src/pin.rs
@@ -1,3 +1,35 @@
+struct _S;
+struct _GenericVal<T>(T);
+
+impl _GenericVal<f32> {}
+
+impl _GenericVal<_S> {}
+
+impl<T> _GenericVal<T> {}
+
+struct Val {
+    val: f64,
+}
+
+struct GenVal<T> {
+    gen_val: T,
+}
+
+impl Val {
+    fn value(&self) -> &f64 {
+        &self.val
+    }
+}
+
+impl<T> GenVal<T> {
+    fn value(&self) -> &T {
+        &self.gen_val
+    }
+}
+
 fn main() {
-    
+    let x = Val { val: 3.0 };
+    let y = GenVal { gen_val: 3i32 };
+
+    println!("{} {} ", x.value(), y.value());
 }
