b4a4aaf9555ad2e30ae57dda590cc0b7b0b00009
Sarthak <218388722+Sarthak05-code@users.noreply.github.com>
2026-08-06 18:24:23 +0545

    Feat : repeared character checker

--- Rustwork/src/main.rs
+++ Rustwork/src/main.rs
@@ -1,11 +1,21 @@
-use std::fs::File;
-use std::io::Write;
+use std::{collections::HashMap, io};
 
 fn main() {
-    let mut file = File::create("hello.txt").unwrap();
+    println!("Enter a word: ");
+    let mut input = String::new();
+    io::stdin().read_line(&mut input).unwrap();
+    let mut freq = HashMap::new();
+    for ch in input.trim().chars() {
+        *freq.entry(ch).or_insert(0) += 1;
+    }
 
-    file.write_all(b"Hello Rust!")
-        .unwrap();
+    let mut repeated = 0;
 
-    println!("Written successfully.");
+    for (ch, count) in &freq {
+        if *count > 1 {
+            println!("{} appears {} times", ch, count);
+            repeated += 1;
+        }
+    }
+    println!("Repeated character {repeated}\n");
 }
