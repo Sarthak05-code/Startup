60389dd14b39a6ded1ce73dc7e485ddaf96689ce
Sarthak <218388722+Sarthak05-code@users.noreply.github.com>
2026-08-09 17:27:38 +0545

    Not a practical way to write impl + struct + traits

--- Rustwork/src/main.rs
+++ Rustwork/src/main.rs
@@ -1,21 +1,26 @@
-use std::{collections::HashMap, io};
+#![allow(warnings)]
+use std::f64::consts::PI;
+
+struct Shapes {
+    radius: f64,
+    length: f64,
+    breadth: f64,
+}
+trait Areas {
+    fn area(&self) -> f64;
+}
+
+impl Areas for Shapes {
+    fn area(&self) -> f64 {
+        PI * self.radius * self.radius
+    }
+}
 
 fn main() {
-    println!("Enter a word: ");
-    let mut input = String::new();
-    io::stdin().read_line(&mut input).unwrap();
-    let mut freq = HashMap::new();
-    for ch in input.trim().chars() {
-        *freq.entry(ch).or_insert(0) += 1;
-    }
-
-    let mut repeated = 0;
-
-    for (ch, count) in &freq {
-        if *count > 1 {
-            println!("{} appears {} times", ch, count);
-            repeated += 1;
-        }
-    }
-    println!("Repeated character {repeated}\n");
+    let circle = Shapes {
+        radius: 12.0,
+        length: 12.0,
+        breadth: 12.0,
+    };
+    println!("{:.2} is the area", circle.area());
 }
