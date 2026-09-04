a66e1bc9e76808f7c9cd347fa3679e8555569bc2
Sarthak <218388722+Sarthak05-code@users.noreply.github.com>
2026-09-04 17:31:27 +0545

    feat : type converstion in rust

--- Rustwork/src/main.rs
+++ Rustwork/src/main.rs
@@ -1,19 +1,47 @@
-#[allow(dead_code , non_snake_case)]
-enum Status {
-    Pending,
-    Accpeted,
-    Rejected,
+#[allow(dead_code)]
+enum Base {
+    Binary,
+    Deciaml,
+    Octal,
+    Hexadecimal,
 }
-fn Name_caller(x : i32) -> i32 {
-    0
+impl Base {
+    fn radix(self) -> u32 {
+        match self {
+            Base::Binary => 2,
+            Base::Deciaml => 10,
+            Base::Octal => 8,
+            Base::Hexadecimal => 16,
+        }
+    }
+}
+
+fn to_base(number: usize, base: Base, buffer: &mut [u8]) -> &str {
+    let radix = base.radix() as usize;
+    const DIGITS: &[u8] = b"0123456789abcdef";
+    if number == 0 {
+        buffer[0] = b'0';
+        return std::str::from_utf8(&buffer[..1]).unwrap();
+    }
+    let mut temp = number;
+    let mut index = 0;
+
+    while temp > 0 {
+        buffer[index] = DIGITS[temp % radix];
+        temp /= radix;
+        index += 1;
+    }
+
+    buffer[..index].reverse();
+    std::str::from_utf8(&buffer[..index]).unwrap()
 }
 
 fn main() {
-    let status = Status::Pending;
+    let mut buffer = [0u8; 64];
+    let binary = to_base(123, Base::Binary, &mut buffer);
+    println!("Binary = {binary}");
 
-    match status {
-        Status::Accpeted => println!("Your package was accepted"),
-        Status::Pending => println!("Your package is pending"),
-        Status::Rejected => println!("Your package was rejeted"),
-    }
+    let mut buffer2 = [0u8; 64];
+    let hex = to_base(2553, Base::Hexadecimal, &mut buffer2);
+    println!("The hex value is {hex}");
 }
