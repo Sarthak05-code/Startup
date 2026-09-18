2fd8f1314e905adc2e6f3d2a7c900273c0d89080
Sarthak <218388722+Sarthak05-code@users.noreply.github.com>
2026-09-18 18:19:10 +0545

    doner

--- Rustwork/src/main.rs
+++ Rustwork/src/main.rs
@@ -1,24 +1,20 @@
-use std::io;
+struct Login {
+    username: String,
+}
+
+trait Data {
+    fn show_data(&self);
+}
+
+impl Data for Login {
+    fn show_data(&self) {
+        println!("Hello, {}", self.username);
+    }
+}
 
 fn main() {
-    println!("Enter a word. ");
-    let mut char1 = String::new();
-    io::stdin().read_line(&mut char1).unwrap();
-    let char1 = char1.trim();
-    println!("Enter a second word. ");
-    let mut char2 = String::new();
-    io::stdin().read_line(&mut char2).unwrap();
-    let char2 = char2.trim();
-
-    let mut byte1: Vec<char> = char1.chars().collect();
-    let mut byte2: Vec<char> = char2.chars().collect();
-
-    byte1.sort();
-    byte2.sort();
-
-    if byte1 == byte2 {
-        println!("The {char1} and {char2} are anagram. ");
-    } else {
-        println!("The {char1} and {char2} aren't anagram");
-    }
+    let login = Login {
+        username: String::from("Sarthak"),
+    };
+    login.show_data();
 }
